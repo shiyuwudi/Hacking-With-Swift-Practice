e285d8da53af154360d2bff5f2f0db0cb65a5bf4
shiyuwudi <289052669@qq.com>
2016-02-26 23:08:10 +0800

    fast view dict genaration
    use run time methods to make view dict which is a replacement for objc
    “NSDictionaryOfVariableBindings” macro

--- Code For Auto Layout/Code For Auto Layout/ViewController.swift
+++ Code For Auto Layout/Code For Auto Layout/ViewController.swift
@@ -9,31 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let label1 = UILabel()
+    let label2 = UILabel()
+    let label3 = UILabel()
+    let label4 = UILabel()
+    let label5 = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let label1 = UILabel()
+
         label1.translatesAutoresizingMaskIntoConstraints = false
         label1.backgroundColor = UIColor.redColor()
         label1.text = "THESE"
         
-        let label2 = UILabel()
         label2.translatesAutoresizingMaskIntoConstraints = false
         label2.backgroundColor = UIColor.cyanColor()
         label2.text = "ARE"
         
-        let label3 = UILabel()
         label3.translatesAutoresizingMaskIntoConstraints = false
         label3.backgroundColor = UIColor.yellowColor()
         label3.text = "SOME"
         
-        let label4 = UILabel()
         label4.translatesAutoresizingMaskIntoConstraints = false
         label4.backgroundColor = UIColor.greenColor()
         label4.text = "AWESOME"
         
-        let label5 = UILabel()
         label5.translatesAutoresizingMaskIntoConstraints = false
         label5.backgroundColor = UIColor.orangeColor()
         label5.text = "LABELS"
@@ -44,7 +46,8 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        let viewsDictionary = dictForViews([label1,label2,label3,label4,label5])
         
         for label in viewsDictionary.keys {
             view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
@@ -61,6 +64,28 @@ class ViewController: UIViewController {
     
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func dictForViews(views:[UIView]) -> [String : UIView] {
+        var count:UInt32 = 0
+        var dict = [String : UIView]()
+        //self.classForCoder:控制器类名
+        //class_copyIvarList:取得类对应实例变量(指针)的数组
+        let ivars = class_copyIvarList(self.classForCoder, &count)
+        for var i = 0; i < Int(count); ++i{
+            //object_getIvar:取得实例变量
+            let ivar = ivars[i]
+            let obj = object_getIvar(self, ivar)
+            guard let view = obj as? UIView, name = String.fromCString(ivar_getName(ivar)) else {
+                break
+            }
+            //过滤其他变量
+            if views.contains(view) {
+                dict[name] = view
+            }
+        }
+        free(ivars)
+        return dict
     }
 
 }
