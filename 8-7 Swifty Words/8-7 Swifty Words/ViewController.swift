a4eb3c7bbc8aed75e7c15ef8c4d26b3abdc8c608
shiyuwudi <289052669@qq.com>
2016-03-01 20:46:53 +0800

    UI layout

---
+++ 8-7 Swifty Words/8-7 Swifty Words/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  8-7 Swifty Words
+//
+//  Created by apple2 on 16/2/28.
+//  Copyright © 2016年 shiyuwudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
+    
+    
+    @IBAction func submitTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func clearTapped(sender: AnyObject) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: "letterTapped:", forControlEvents: .TouchUpInside)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
